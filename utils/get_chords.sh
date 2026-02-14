0b4e12172db0396652c6ae50848d50ce0d8abfd6
Georg Pernice <georgpernice@gmail.com>
2026-02-14 04:43:35 +0100

    update

--- utils/get_chords.sh
+++ utils/get_chords.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 url=$1
 echo $url 
-if [[ "$url" =~ ([0-9]{2,8})$ ]]; then
-    echo "Domain name: ${BASH_REMATCH[1]}"
+if [[ "$url" =~ ([0-9]{2,16})$ ]]; then
+    echo "Song ID: ${BASH_REMATCH[1]}"
     id=${BASH_REMATCH[1]} 
 else
     echo "Invalid URL"
     exit 1
 fi
 
-echo "song id is: ${id} i think."
 if [ -f songs/latest.txt ]; then echo overwrite latest song..; else touch songs/latest.txt; fi
 echo -e "\033[44m Close ddgr with q and your song will be displayed."
 rm songs/latest.txt
