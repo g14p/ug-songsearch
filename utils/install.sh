27b11a0273efe2a2ab7763871e0bbcb66c3eb419
Georg Pernice <georgpernice@gmail.com>
2026-02-14 15:09:10 +0100

    update installation

--- utils/install.sh
+++ utils/install.sh
@@ -1,9 +1,8 @@
 echo we will install requirements. You need to run this with sudo. Okay?
 echo Requirements: ultimate-guitar-scraper, golang, ddgr
 echo "(y or n)"
+
 read consent
-
-
 if [ $consent == "y" ]  then 
     git clone https://github.com/Pilfer/ultimate-guitar-scraper.git 
     cd ultimate-guitar-scraper 
@@ -12,6 +11,17 @@ if [ $consent == "y" ]  then
     sudo apt install ddgr
 else 
     exit 1; 
+fi
+
+echo to make an alias 'ugss' and reach ug-songsearch from every terminal window type: 
+echo "(y / n)"
+
+read consent
+if [ $consent == "y" ]  then 
+    echo 'alias ugss="~/git/ug-songsearch/ugss $@"' >> ~/.bashrc
+else 
+    echo "making no alias. You can call it from the gitfolder as well :)"
+    exit 0; 
 fi
 
 
